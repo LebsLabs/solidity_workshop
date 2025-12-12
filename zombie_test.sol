2277739121f05f11019b6bd747c70407601c6a79
LebsLabs <alen.leban@gmail.com>
2025-12-12 10:58:00 +0100

    Update zombie_test.sol

--- zombie_test.sol
+++ zombie_test.sol
@@ -4,23 +4,23 @@ contract ZombieFactory { //program
 
     event NewZombie(uint zombieId, string name, uint dna); //call the program to run
 
-    uint dnaDigits = 16;
+    uint dnaDigits = 16;    //the number will have 16 digits 
     uint dnaModulus = 10 ** dnaDigits; //making sure that the number has 16 digits
 
-    struct Zombie {
+    struct Zombie {    //label map Zombie 
         string name;    //string = word
         uint dna;       //uint = number, variable
     }
 
-    Zombie[] public zombies;
+    Zombie[] public zombies; //indexing zombies
 
-    mapping (uint => address) public zombieToOwner;
-    mapping (address => uint) ownerZombieCount;
+    mapping (uint => address) public zombieToOwner; //mapping numbers to addresses
+    mapping (address => uint) ownerZombieCount;    //mapping addresses to numbers
 
     function _createZombie(string memory _name, uint _dna) private {
         uint id = zombies.push(Zombie(_name, _dna)) - 1;
-        zombieToOwner[msg.sender] = id; 
-        uint id = ++; // start here
+        zombieToOwner[id] = msg.sender; 
+        ownerZombieCount[msg.sender]++;
         emit NewZombie(id, _name, _dna);
     }
 
@@ -35,4 +35,5 @@ contract ZombieFactory { //program
     }
 
 }
+
 
