065318fba9083876e97e6dba330fd49aa9bd6e51
LebsLabs <alen.leban@gmail.com>
2025-12-18 19:59:16 +0100

    Update ZombieFeeding contract with new functions
    Refactor feedAndMultiply to accept parameters and add feedOnKitty function.

--- zombiefeeding_test.sol
+++ zombiefeeding_test.sol
@@ -19,15 +19,24 @@ contract KittyInterface {
 
 contract ZombieFeeding is ZombieFactory {
 
-    address ckAddress = //write a made up address key ;
-    KittyInterface kittyContract = KittyInterface(ckAddress); 
+address ckAddress = //write a made up address key ;
+KittyInterface kittyContract = KittyInterface(ckAddress); 
 
-    function feedAndMultiply() public {
+function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) public {
         require(msg.sender == zombieToOwner[_zombieId]);
         Zombie storage myZombie = zombies[_zombieId];
         _targetDna = _targetDna % dnaModulus; 
         uint newDna = (myZombie.dna + _targetDna) / 2;
+        if (keccak256(abi.encodePacked(_species)) == keccak256(abi.encodePacked("kitty"))) {
+            newDna = newDna - newDna % 100 + 99; 
+        }
         _createZombie("NoName", newDna); 
+    }
+
+    function feedOnKitty(uint _zombieId, uint _kittyId) public {
+        uint KittyDna; 
+        (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
+        feedAndMultiply(_zombieId, kittyDna, "kitty"); 
     }
 
 } 
