e90c21c27c59d44e8f2b5a4f915cc2bcc9f2309e
LebsLabs <alen.leban@gmail.com>
2025-12-25 18:58:11 +0100

    NatSpec
    Updated comments and documentation for clarity and consistency.

--- zombie_test.sol
+++ zombie_test.sol
@@ -1,41 +1,82 @@
-pragma solidity >=0.5.0 <0.6.0; //version of pragma
+//**
+* @dev This is pragma version.
+*/
+pragma solidity >=0.5.0 <0.6.0; 
 
-contract ZombieFactory { //program
+//** 
+* @dev Our smart contrat which is the program that we will write. 
+* @param Event calls our little progarm to run.
+*/
+contract ZombieFactory { 
+    event NewZombie(uint zombieId, string name, uint dna); 
 
-    event NewZombie(uint zombieId, string name, uint dna); //call the program to run
+    //**
+    * @dev The number will have 16 digits.
+    * @dev It will make sure that the number has really 16 digits.
+    */
+    uint dnaDigits = 16;    
+    uint dnaModulus = 10 ** dnaDigits; 
 
-    uint dnaDigits = 16;    //the number will have 16 digits 
-    uint dnaModulus = 10 ** dnaDigits; //making sure that the number has 16 digits
-
-    struct Zombie {    //label map Zombie 
-        string name;    //string = word
-        uint dna;       //uint = number, variable
+    //** 
+    * @param Struct Zombie is like a map with label Zombie.
+    * @param The string is a word.
+    * @param The uint is a number or a var in python.
+    */
+    struct Zombie {    
+        string name;   
+        uint dna;      
     }
 
-    Zombie[] public zombies; //indexing zombies
+    //**
+    * @dev With arrayys we do indexing of zombies. 
+    */
+    Zombie[] public zombies;
 
-    mapping (uint => address) public zombieToOwner; //mapping numbers to addresses
-    mapping (address => uint) ownerZombieCount;    //mapping addresses to numbers
+    //**
+    * @param Uint will be mapped to the address.
+    * @param Address will be mapped to a number. 
+    */
+    mapping (uint => address) public zombieToOwner; 
+    mapping (address => uint) ownerZombieCount;    
 
-    function _createZombie(string memory _name, uint _dna) private {    //a function named createZombie that receives _name and _dna and is private so only this contract can call it 
-        uint id = zombies.push(Zombie(_name, _dna)) - 1;    //field of zombie structures that creates a new zombie with _name and _dna and if the new lenght of array becomes 1 the index of the new zombie will be always 0    
-        zombieToOwner[id] = msg.sender; //the key-value pair zombieToOwner saves the ownership of id to the msg.sender
-        ownerZombieCount[msg.sender]++;    //adds the plus 1 zombie to the sender who owns the zombies
-        emit NewZombie(id, _name, _dna);    //send the this information on blockchain... where is readable to others eg. web3.js
+    //**
+    * @dev Function createZombie with arguments _name, _dna is private and only this contract, program can call it. 
+    * @dev uint id is the field that create a new zombie with _name, _dna and if the lenght of array becomes 1, it will be taken to index 0.  
+    * @dev zombieToOwner saves the ownership of id to the msg.sender. 
+    * @param The ++ adds the new zombie to the sender who owns the zombies. 
+    * @param Emit this information on blockchain where is readable to others eg. file.js. 
+    */
+    function _createZombie(string memory _name, uint _dna) private {    
+        uint id = zombies.push(Zombie(_name, _dna)) - 1;       
+        zombieToOwner[id] = msg.sender; 
+        ownerZombieCount[msg.sender]++;    
+        emit NewZombie(id, _name, _dna);   
     }
 
-    function _generateRandomDna(string memory _str) private view returns (uint) {    //this f(x) does change zero on blockchain, only readable within the contract, the name that user types 
-        uint rand = uint(keccak256(abi.encodePacked(_str)));    //variable rand stores variable of a big number that converts the result of calculation of SHA-3 (256-bits number or 32 bytes), abi. ...
-        return rand % dnaModulus;    //% operation for modul, dnaModulus is defined at the beginning of the contract, so the function returns variable rand limited to dnaModulus number of digits
+    //**
+    * @param _generateRandomDna does zero change on blockchain, only readable within the contract. Returns the name that the user types.
+    * @param Variable rand stores variable of a big number that converts result of calculation of SHA-3 (256-bits number or 32 bytes). 
+    * @param Operation modulus, %, returns variable rand limites to dnaModulus number of digits (dnaModulus is defined at the beginning). 
+    */
+    function _generateRandomDna(string memory _str) private view returns (uint) {    
+        uint rand = uint(keccak256(abi.encodePacked(_str)));  
+        return rand % dnaModulus;    
     }
 
-    function createRandomZombie(string memory _name) public {    //f(craeteRandomZombie) when user types name and f() is readable to anyone with eth address
-        require(ownerZombieCount[msg.sender] == 0);      //condition that the messagesender owns zero zombies
-        uint randDna = _generateRandomDna(_name);    //calls the f(generateRandomDna and based on the name calculate the dna number and stores it in randDna
-        _createZombie(_name, randDna);    //calls the second f() that adds the new zombie and set the ownership and rise the number of zombies of the user and starts the event
+    //**
+    * @param Function createRandomZombie when user types name of the zombie this function is readable to anyone with Eth address.
+    * @param Function requires that the zombies count of the owner who is the sender starts with 0, or function does not call functions. 
+    * @dev It calls the function generateRandomDna calculates based on the typed name the number and stores it in randDna.
+    * @dev It calls the second function that addds the new zombie to the ___, sets the ownership and rises the number of user zombies and starts the event. 
+    */
+    function createRandomZombie(string memory _name) public {    
+        require(ownerZombieCount[msg.sender] == 0);      
+        uint randDna = _generateRandomDna(_name);    
+        _createZombie(_name, randDna);    
     }
 
 }
+
 
 
 
